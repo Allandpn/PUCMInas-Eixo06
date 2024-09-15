0ccc19e25fb67e7f2edfd5c9016c3785dd7ba77b
Allandpn <99135710+Allandpn@users.noreply.github.com>
2024-09-15 12:00:14 -0300

    Fisrt Commit

---
+++ mf-api-web-services/mf-api-web-services/Models/Veiculo.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_api_web_services.Models
+{
+    [Table ("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Marca { get; set; }
+
+        [Required]
+        public string Modelo { get; set; }
+
+        [Required]
+        public int AnoFabricacao { get; set; }
+
+        [Required]
+        public int AnoModelo { get; set; }
+
+        public ICollection<Consumo> Consumos { get; set; }
+    }
+}
