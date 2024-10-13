f082ac702f9d348d22681a360a882bc24c301dc5
Allandpn <99135710+Allandpn@users.noreply.github.com>
2024-10-13 16:24:51 -0300

    inicio rota agendamento

--- src/webpage/src/pages/agendamentos/AgendamentoDetails.jsx
+++ src/webpage/src/pages/agendamentos/AgendamentoDetails.jsx
@@ -1,9 +1,40 @@
-const AgendamentoDetails = () => {
-  return (
-    <div>
-      <h3>Detalhes Agendamento</h3>
-    </div>
-  );
-};
+import { useParams, Link } from "react-router-dom";
+import { Card, Button, Container } from "react-bootstrap";
+import { useFetch } from "../../hooks/useFetch";
 
-export default AgendamentoDetails;
+export default function AgendamentoDetails() {
+  const { id } = useParams();
+  //console.log(id);
+  const url = "https://localhost:5005/agendamentos/" + id;
+  const { data: agendamento, loading, error } = useFetch(url);
+  //console.log({ agendamento });
+  return (
+    <Container className="mt-4">
+      {error && <p>Ocorreu um erro...</p>}
+      {loading && <p>Carregando produto...</p>}
+      {agendamento && (
+        <Card>
+          <Card.Body>
+            <Card.Title>Detalhes do agendamento</Card.Title>
+            <Card.Text>
+              <br />
+              <strong>Nome:</strong> {agendamento.nomePaciente}
+              <br />
+              <strong>Email:</strong> {agendamento.email}
+              <br />
+              <strong>Data:</strong> {agendamento.dataAtendimento}
+              <br />
+              <br />
+              <strong>Email Medico:</strong>{" "}
+              {agendamento.emailMedicoResponsavel}
+              <br />
+            </Card.Text>
+            <Link to="/agendamentos">
+              <Button variant="secondary">Voltar</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+      )}
+    </Container>
+  );
+}
--- src/webpage/src/pages/agendamentos/Agendamentos.jsx
+++ src/webpage/src/pages/agendamentos/Agendamentos.jsx
@@ -1,13 +1,59 @@
+import { useFetch } from "../../hooks/useFetch";
 import { Link } from "react-router-dom";
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
 
 Link;
 
 const Agendamentos = () => {
+  const url = "https://localhost:5005/agendamentos/";
+  const { data: agendamentos, httpConfig, loading, error } = useFetch(url);
+
+  const handleDelete = (id) => {
+    httpConfig(id, "DELETE");
+  };
+
+  //console.log(agendamento);
   return (
-    <div>
-      <h1>Agendamentos</h1>
-      <Link to={`/agendamento/${1}`}>Detalhes</Link>
-    </div>
+    <Container className="mt-4">
+      <h1 className="text-center mb-4">Agendamentos</h1>
+
+      {error && <p className="text-danger text-center">Ocorreu um erro...</p>}
+      {loading && <p className="text-center">Carregando agendamento...</p>}
+
+      <Row>
+        {agendamentos &&
+          agendamentos.map((agendamento) => (
+            <Col key={agendamento.id} sm={12} md={6} lg={4} className="mb-4">
+              <Card>
+                <Card.Body>
+                  <Card.Title></Card.Title>
+                  <Card.Text>
+                    <strong>Paciente:</strong> {agendamento.nomePaciente}
+                  </Card.Text>
+                  <Card.Text>
+                    <strong>Email:</strong> {agendamento.email}
+                  </Card.Text>
+                  <Card.Text>
+                    <strong>Data:</strong> {agendamento.dataAtendimento}
+                  </Card.Text>
+                  <div className="d-flex justify-content-between">
+                    <Link to={`/agendamento/${agendamento.id}`}>
+                      <Button variant="primary">Ver Detalhes</Button>
+                    </Link>
+                    <Button variant="warning">Editar</Button>
+                    <Button
+                      variant="danger"
+                      onClick={() => handleDelete(agendamento.id)}
+                    >
+                      Excluir
+                    </Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+      </Row>
+    </Container>
   );
 };
 
